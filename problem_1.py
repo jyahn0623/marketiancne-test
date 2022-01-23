0c77299ba16771a42c9a0a9dc6b1fdda9692e1f8
paulahn <jyahn0623@gmail.com>
2022-01-23 20:08:16 +0900

    commit

--- problem_1.py
+++ problem_1.py
@@ -1,6 +1,21 @@
 
-def problem():
+def problem_1():
     pass
+
+def problem_2(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
     
 if __name__ == '__main__':
-    problem()
+    problem_1()
+    problem_2(["12","123","1235","567","88"])
+    problem_2(["123","456","789"])
